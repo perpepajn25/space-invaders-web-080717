3ea25c4d6629c80405bac04414e78c09b08954bf
mgaspari <marcoanthonygaspari@gmail.com>
2017-09-20 16:44:53 -0400

    Done

--- crewMember.js
+++ crewMember.js
@@ -7,15 +7,24 @@ class CrewMember {
     if (this.position !== "Pilot" || this.currentShip == "Looking for a Rig"){
       return "had no effect"
     }
+    else {
+      this.currentShip.warpDrive = 'engaged!'
+    }
    }
   setsInvisibility(){
     if (this.position !== "Defender" || this.currentShip == "Looking for a Rig"){
       return "had no effect"
     }
+    else {
+      this.currentShip.cloaked = true
+    }
   }
   chargePhasers(){
     if (this.position !== "Gunner" || this.currentShip == "Looking for a Rig"){
       return "had no effect"
+    }
+    else {
+      this.currentShip.phasersCharge = 'charged!'
     }
   }
 }
--- spaceship.js
+++ spaceship.js
@@ -1,5 +1,21 @@
 class Spaceship{
-  constructor(name){
+  constructor(name, crew, phasers, shields){
     this.name = name
+    this.crew = crew
+    this.phasers = phasers
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = "disengaged"
+    this.docked = this.testDocked()
+    this.phasersCharge = 'uncharged'
+    this.addMembers()
+  }
+
+  testDocked(){
+    return this.crew.length === 0 ? true : false
+  }
+
+  addMembers(){
+    this.crew.forEach(member => member.currentShip = this)
   }
 }
